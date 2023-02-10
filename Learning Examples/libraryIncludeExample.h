f0f8d979de8166733c527fbb145c98ab52686ead
doctor3140 <doctor3140@gmail.com>
2023-02-10 17:10:58 -0500

    Made includeExample work with library and changed location to examples. Need to still add text.

---
+++ Learning Examples/libraryIncludeExample.h
@@ -0,0 +1,9 @@
+bool equals(int *arr1, int *arr2) //compares arrays of size three
+{
+	if((arr1[0]==arr2[0])&&(arr1[1]==arr2[1])&&(arr1[2]==arr2[2])){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
