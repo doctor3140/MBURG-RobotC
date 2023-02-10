f5488ab05f47db3dbeb0314a7bc21d21189c555b
doctor3140 <doctor3140@gmail.com>
2023-02-10 11:18:41 -0500

    added basic file structure and latest files from drive.

---
+++ Learning Examples/README.md
@@ -0,0 +1,2 @@
+# MBURG-RobotC
+Library and Competition Code from Mercersburg Robotics PGA. Includes code, examples, competetion documentation, robot designs, and stls.
---
+++ Learning Examples/StringExample.c
@@ -0,0 +1,12 @@
+task main(){
+	string str1 = "hello";
+	string str2 = "hello";
+
+	if(StringFind(str1, "hello">=0)){
+		playTone();
+	}
+	else{
+	motor[motorC];
+	}
+}
+
