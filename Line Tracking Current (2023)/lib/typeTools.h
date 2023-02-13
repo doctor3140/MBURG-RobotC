aaf69186abbd94b3b30d9c720dc8766886acd074
doctor3140 <doctor3140@gmail.com>
2023-02-13 17:08:46 -0500

    ColorRawPID is angry still, but fixed String example. Made other changes in some lib files.

--- Line Tracking Current (2023)/lib/typeTools.h
+++ Line Tracking Current (2023)/lib/typeTools.h
@@ -1,4 +1,13 @@
-//compares arrays of any size
+int array1;
+int array2;
+
+void setArrays(int *arr1, int *arr2){
+	array1 = arr1;
+	array2 = arr2;
+}
+
+
+//Array and String Functions
 bool equals(int *arr1, int *arr2, int size){
 	int equal = 0;
 	for(int i = 0; i<size; i++){
@@ -13,21 +22,34 @@ bool equals(int *arr1, int *arr2, int size){
 		return false;
 	}
 }
+//compares arrays of any size
 
-//compares arrays of size three
-
-bool equals(int *arr1, int *arr2) {
+bool equals(int *arr1, int *arr2) //compares arrays of size three
+{
 	if((arr1[0]==arr2[0])&&(arr1[1]==arr2[1])&&(arr1[2]==arr2[2])){
 		return true;
 	}
 	else{
 		return false;
 	}
-	
-bool equals(string str1, string str2){
-	StringFind(str1, str2>=0)
-} 
+}
 
-//void calculateArrDiff(int *arr1, int *arr2, int size){
+/*
+bool equals() //compares sensor to target
+{
+	if((target[0]==current[0])&&(target[1]==current[1])&&(target[2]==current[2])){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+*/
+
+bool equals(string str1, string str2){
+	return (stringFind(str1, str2)>=0);
+}
+
+//void calculate(int *arr1, int *arr2, int size){
 //		for
 //}
