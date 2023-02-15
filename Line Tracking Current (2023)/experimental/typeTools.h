76f6fdc3c593cdde54e92f2ece696502d8861da9
doctor3140 <doctor3140@gmail.com>
2023-02-15 15:50:14 -0500

    issue with equals function. Made test program to find out why. Hate this.

---
+++ Line Tracking Current (2023)/experimental/typeTools.h
@@ -0,0 +1,44 @@
+int array1;
+int array2;
+
+void setArrays(int *arr1, int *arr2){
+	array1 = arr1;
+	array2 = arr2;
+}
+
+
+//Array and String Functions
+bool equals(int *arr1, int *arr2, int size){
+	int equal = 0;
+	for(int i = 0; i<size; i++){
+		if(arr1[i]==arr2[i]){
+			equal++;
+		}
+	}
+	if(equal==size){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+//compares arrays of any size
+
+bool equals(int *arr1, int *arr2) //compares arrays of size three
+{
+	if((arr1[0]==arr2[0])&&(arr1[1]==arr2[1])&&(arr1[2]==arr2[2])){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+
+bool equals(string str1, string str2){
+	return (stringFind(str1, str2)>=0);
+}
+
+//void calculate(int *arr1, int *arr2, int size){
+//		for
+//}
