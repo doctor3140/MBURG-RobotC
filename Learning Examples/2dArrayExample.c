f61600c1207af404f7f2864f35e5094bbc5536de
doctor3140 <doctor3140@gmail.com>
2023-02-24 17:07:08 -0500

    added 2d arrays stuff

--- Learning Examples/2dArrayExample.c
+++ Learning Examples/2dArrayExample.c
@@ -1,2 +1,18 @@
-//https://www.vexforum.com/t/header-files-robotc/43269
+// http://robodesigners.blogspot.com/2012/04/arrays-in-robotc.html
 
+int arr[2][3]= {
+		{10, 11, 12},
+	  {14, 15, 16}
+};
+
+//passing as argument has caused issues
+bool test(){
+	if(arr[0][0] == 10){
+		return true;
+	}
+		return false;
+}
+
+task main(){
+	test();
+}
---
+++ Line Tracking Current (2023)/lib/RawColorTools.h
@@ -0,0 +1,105 @@
+int currentRight[3]= {
+		{0, 0, 0}
+};
+int currentLeft[3]= {
+		{0, 0, 0}
+};
+
+int black[2][3]= {
+		{32, 41, 20},
+	  {34, 43, 22}
+};
+int white[2][3]= {
+		{356, 396, 196},
+	  {359, 396, 196}
+};
+int green[2][3]= {
+		{71, 183, 35},
+	  {73, 185, 36}
+};
+int red[2][3]= {
+		{262, 33, 16},
+	  {264, 33, 17}
+};
+
+//might add setter methods if deemed necessary, but honestly seems kinda annoying tbh
+
+bool rightEqualsBlack(){
+getColorRawRGB(rightS, currentRight[0], currentRight[1], currentRight[2]);
+if((black[0][0]<=currentRight[0]<=black[1][0])&&(black[0][1]<=currentRight[1]<=black[1][2])&&(black[0][2]<=currentRight[2]<=black[1][2])){
+	return true;
+}
+else{
+	return false;
+}
+}
+
+bool leftEqualsBlack(){
+getColorRawRGB(leftS, currentRight[0], currentRight[1], currentRight[2]);
+if((black[0][0]<=currentLeft[0]<=black[1][0])&&(black[0][1]<=currentLeft[1]<=black[1][2])&&(black[0][2]<=currentLeft[2]<=black[1][2])){
+	return true;
+}
+else{
+	return false;
+}
+}
+
+bool rightEqualsWhite(){
+getColorRawRGB(rightS, currentRight[0], currentRight[1], currentRight[2]);
+if((white[0][0]<=currentRight[0]<=white[1][0])&&(white[0][1]<=currentRight[1]<=white[1][2])&&(white[0][2]<=currentRight[2]<=white[1][2])){
+	return true;
+}
+else{
+	return false;
+}
+}
+
+bool leftEqualsWhite(){
+getColorRawRGB(leftS, currentRight[0], currentRight[1], currentRight[2]);
+if((white[0][0]<=currentLeft[0]<=white[1][0])&&(white[0][1]<=currentLeft[1]<=white[1][2])&&(white[0][2]<=currentLeft[2]<=white[1][2])){
+	return true;
+}
+else{
+	return false;
+}
+}
+
+bool rightEqualsGreen(){
+getColorRawRGB(rightS, currentRight[0], currentRight[1], currentRight[2]);
+if((green[0][0]<=currentRight[0]<=green[1][0])&&(green[0][1]<=currentRight[1]<=green[1][2])&&(green[0][2]<=currentRight[2]<=green[1][2])){
+	return true;
+}
+else{
+	return false;
+}
+}
+
+bool leftEqualsGreen(){
+getColorRawRGB(leftS, currentRight[0], currentRight[1], currentRight[2]);
+if((green[0][0]<=currentLeft[0]<=green[1][0])&&(green[0][1]<=currentLeft[1]<=green[1][2])&&(green[0][2]<=currentLeft[2]<=green[1][2])){
+	return true;
+}
+else{
+	return false;
+}
+}
+
+bool rightEqualsRed(){
+getColorRawRGB(rightS, currentRight[0], currentRight[1], currentRight[2]);
+if((red[0][0]<=currentRight[0]<=red[1][0])&&(red[0][1]<=currentRight[1]<=red[1][2])&&(red[0][2]<=currentRight[2]<=red[1][2])){
+	return true;
+}
+else{
+	return false;
+}
+}
+
+bool leftEqualsRed(){
+getColorRawRGB(leftS, currentRight[0], currentRight[1], currentRight[2]);
+if((red[0][0]<=currentLeft[0]<=red[1][0])&&(red[0][1]<=currentLeft[1]<=red[1][2])&&(red[0][2]<=currentLeft[2]<=red[1][2])){
+	return true;
+}
+else{
+	return false;
+}
+}
