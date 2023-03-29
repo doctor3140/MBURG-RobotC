0116c5e66caffb65315cd5ba40067992ff1ad27a
doctor3140 <doctor3140@gmail.com>
2023-03-29 15:56:42 -0400

    added Lawn.c as a way to test out rescue room code

---
+++ Line Tracking Current (2023)/Lawn.c
@@ -0,0 +1,96 @@
+#pragma config(Sensor, S1,     rightS,         sensorEV3_Color, modeEV3Color_RGB_Raw)
+#pragma config(Sensor, S2,     leftS,          sensorEV3_Color, modeEV3Color_RGB_Raw)
+#pragma config(Sensor, S3,     reflect,        sensorEV3_Color)
+#pragma config(Sensor, S4,     sonarSensor,    sensorEV3_Ultrasonic)
+#pragma config(Motor,  motorA,          armMotor,      tmotorEV3_Medium, PIDControl, encoder)
+#pragma config(Motor,  motorB,          leftMotor,     tmotorEV3_Large, PIDControl, reversed, driveLeft, encoder)
+#pragma config(Motor,  motorC,          rightMotor,    tmotorEV3_Large, PIDControl, reversed, driveRight, encoder)
+#pragma config(Motor,  motorD,           ,             tmotorEV3_Large, openLoop)
+
+#include "\lib\teamTools.h"
+#include "lib\RawColorTools.h"
+
+#define UP 0;
+#define DOWN 1;
+#define LEFT 2;
+#define RIGHT 3;
+
+float boxWidth;
+float boxLength;
+float boxMargin;
+
+float robotFront;
+float robotBack;
+float robotSides;
+
+float currentXY[2] = {
+	{0, 0}
+	};
+string direction[4] = {
+	{"Up", "Down", "Left", "Right"}
+};
+
+int
+
+
+setBoxDimensions(float w, float l, float m){
+	boxWidth = w;
+	boxLength = l;
+	boxMargin = m;
+}
+
+setRobotDimensions(float f, float b, float s){
+	robotFront = f;
+	robotBack = b;
+	robotSides = s;
+} //in CM might want to convert to MM ???
+
+
+
+mowRoom(){
+//if(rightEqualsReflect()&&leftEqualsReflect){
+
+	
+}
+	/*
+	mapRoom()
+	find wall to front
+	find wall to left
+	find wall to right
+	
+	//current goal is to make it know the position in a box and navigate
+	*/
+
+	task displayPos(){
+		repeat(forever){
+		displayBigTextLine(2, "SS: %d", SensorValue[S4]);
+		displayBigTextLine(4, "X: %d", currentXY[0]);
+		displayBigTextLine(6, "Y: %d", currentXY[1]);
+		displayBigTextLine(6, "D: %d", direction[]);
+		sleep(10); //refreshrate in ms
+	}
+	}
+
+//}
+
+
+void properties(){
+		//setMotorDirection('f');
+		setWheelDiameterCM(7.455);
+		setUTURN(660);
+		setPoint(332);
+		setSpeed(20);
+		setLeanSpeed(6);
+		setSearchSpeed(3);
+		setDist(2.5);
+		setTapeThreasholdCM(2.5);
+		setSearchSpeed(3);
+		setSearchTime(4000);
+}
+
+task main(){
+	startTask(displayPos);
+	properties();
+	setDimension(90, 120, .1);
+	mowRoom();
+}
